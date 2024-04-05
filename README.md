fb0a4bb173fafd466b1e94d1cd10ef6f7f69ec4b
Darkness <agrawaldeepesh546@gmail.com>
2024-04-06 01:22:47 +0530

    Refactor code to add CRUD operations for FavYoutubeVideosModel

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
+
+CRUD OPERATION USING HONO AND BUN 
+
+
+
 To install dependencies:
 ```sh
 bun install
 ```
 
+Add MONOGO_URI
+
 To run:
 ```sh
 bun run dev
 ```
+
+
+![alt text](image.png)
 
 open http://localhost:3000
--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -3,6 +3,8 @@ import { poweredBy } from "hono/powered-by"
 import { logger } from "hono/logger"
 import dbConnection from './db/connect'
 import FavYoutubeVideosModel, { IFavYoutubeSchema } from './db/fav-youtube-model'
+import { streamText, streamSSE, stream } from "hono/streaming"
+import { isValidObjectId } from 'mongoose'
 
 const app = new Hono()
 
@@ -22,11 +24,99 @@ dbConnection().then(() => {
 
     // Create document
     app.post("/", async (c) => {
-        const formData: IFavYoutubeSchema = await c.req.json()
+        const formData = await c.req.json()
         if (!formData.thumbnailUrl) delete formData.thumbnailUrl
-        
+
+        const FavYoutubeVideosObject = new FavYoutubeVideosModel(formData)
+
+        try {
+            const document = await FavYoutubeVideosObject.save()
+            return c.json(document.toObject(), 201);
+        }
+        catch (error) {
+            return c.json((error as any)?.message || "Internal server Error", 500)
+        }
+
     })
 
+    // View document By Id
+    app.get("/:documentId", async (c) => {
+        const id = c.req.param("documentId")
+        if (!isValidObjectId(id)) return c.json("Invalid ID", 400)
+
+
+        const document = await FavYoutubeVideosModel.findById(id)
+
+        if (!document) return c.json("Document not Found", 404)
+
+        return c.json(document.toObject(), 200)
+    })
+
+
+    app.get("/d/:documentId", async (c) => {
+        const id = c.req.param("documentId")
+        if (!isValidObjectId(id)) return c.json("Invalid ID", 400)
+
+
+        const document = await FavYoutubeVideosModel.findById(id)
+
+        if (!document) return c.json("Document not Found", 404)
+
+        return streamText(c, async (stream) => {
+            stream.onAbort(() => {
+                console.log("Aborted")
+            })
+
+            for (let i = 0; i < document.description.length; i++) {
+                await stream.write(document.description[i])
+                await stream.sleep(4000)
+            }
+        })
+
+    })
+
+
+    app.patch("/:documentId", async (c) => {
+        const id = c.req.param("documentId")
+        if (!isValidObjectId(id)) return c.json("Invalid ID", 400)
+
+
+        const document = await FavYoutubeVideosModel.findById(id)
+
+        if (!document) return c.json("Document not Found", 404)
+
+        const formData: IFavYoutubeSchema = await c.req.json()
+
+        if (!formData.thumbnailUrl) {
+            delete formData.thumbnailUrl
+        }
+        try {
+            const updatedDocument = await FavYoutubeVideosModel.findByIdAndUpdate(
+                id,
+                formData,
+                {
+                    new: true
+                }
+            )
+
+            return c.json(updatedDocument?.toObject(), 200)
+        } catch (error) {
+
+        }
+    })
+
+    app.delete("/:documentId", async (c) => {
+        const id = c.req.param("documentId")
+        if (!isValidObjectId(id)) return c.json("Invalid ID", 400)
+
+        try {
+            const document = await FavYoutubeVideosModel.findByIdAndDelete(id)
+            return c.json(document?.toObject(), 200)
+        } catch (error) {
+            console.error(error)
+            return c.json("error", 500)
+        }
+    })
 }).catch(err => {
     app.get("/*", (c) => {
         return c.text(`failed to connect with mongo db:${err.message}`)
