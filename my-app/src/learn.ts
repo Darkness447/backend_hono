31904d56a49c53f0ad0c4b186a0e0a8177030725
Darkness <agrawaldeepesh546@gmail.com>
2024-04-05 22:53:27 +0530

    add delete and update video request

--- my-app/src/learn.ts
+++ my-app/src/learn.ts
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
 import { v4 as uuidv4 } from "uuid";
+import { streamText, streamSSE, stream } from "hono/streaming"
 
-let videos = []
+type VideoType = {
+    id: string
+    videoName: string
+    channelName: string
+    Duration: string
+}
+
+let videos: VideoType[] = []
 
 const app = new Hono()
 
@@ -10,10 +18,10 @@ app.get("/", (c) => {
     return c.html("<h1>welcome deepesh</h1>")
 })
 
-app.post("/videos",async (c)=>{
-    const {videoName,channelName,Duration} = await c.req.json()
-    const newVideo ={
-        id:uuidv4(),
+app.post("/videos", async (c) => {
+    const { videoName, channelName, Duration } = await c.req.json()
+    const newVideo = {
+        id: uuidv4(),
         videoName,
         channelName,
         Duration
@@ -22,5 +30,47 @@ app.post("/videos",async (c)=>{
     videos.push(newVideo)
     return c.json(newVideo)
 })
+
+app.get("/videos", (c) => {
+    return streamText(c, async (stream) => {
+        for (const element of videos) {
+            await stream.writeln(JSON.stringify(element))
+            await stream.sleep(4000)
+        }
+    })
+})
+
+// Read all data using stream
+
+app.get("/video/:id", (c) => {
+    const { id } = c.req.param()
+    const video = videos.find((video) => video.id === id);
+
+    if (!video) {
+        return c.json(({ message: "Video Not Found" }))
+    }
+    return c.json(video)
+})
+
+
+// update also
+
+app.put("/video/:id", async (c) => {
+    const { id } = c.req.param()
+    const index = videos.findIndex((video) => video.id === id)
+
+    if (index === -1) {
+        return c.json(({ message: "Video Not Found" }), 404)
+    }
+
+    const { videoName, channelName, Duration } = await c.req.json()
+
+    videos[index] = { ...videos[index], videoName, channelName, Duration };
+
+    return c.json(videos[index])
+})
+
+
+// deelte
 
 export default app
